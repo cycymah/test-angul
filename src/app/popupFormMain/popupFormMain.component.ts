2e0f14bde9170ac2fa020fccbecae04f684073b6
Ivan Solnyshkin <ethosy@yandex.ru>
2020-10-21 18:56:48 +0700

    feat: Начало реализации открытия модалок

--- src/app/popupFormMain/popupFormMain.component.ts
+++ src/app/popupFormMain/popupFormMain.component.ts
@@ -5,4 +5,12 @@ import { Component } from '@angular/core';
   templateUrl: './popupFormMain.component.html',
   styleUrls: ['./popupFormMain.component.css'],
 })
-export class popupFormMain {}
+export class popupFormMain {
+  isOpen: boolean = true;
+
+  handlerPopupClose() {
+    console.log(this.isOpen);
+    this.isOpen = true;
+    console.log(this.isOpen);
+  }
+}
